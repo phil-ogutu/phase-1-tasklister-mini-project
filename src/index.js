c503ab7c50750062b1f107d22263832b2c77e630
phil-ogutu <phil.shifwoko@gmail.com>
2025-03-16 22:29:09 +0300

    Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+  let formInput = document.querySelector("#new-task-description")
+  let taskList = document.querySelector("#tasks")
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let taskText = formInput.value.trim()
+    let taskItem = document.createElement("li")
+
+    taskItem.textContent = taskText;
+    taskList.appendChild(taskItem)
+    event.target.reset()
+
+    let deleteButton = document.createElement("button")
+    deleteButton.innerText = "X"
+    deleteButton.style.marginLeft = "10px"
+    deleteButton.addEventListener("click", () => {
+      taskList.removeChild(taskItem)
+    })
+
+    taskItem.appendChild(deleteButton);
+
+  });
 });
